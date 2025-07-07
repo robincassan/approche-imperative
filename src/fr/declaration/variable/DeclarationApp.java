e90767bea526add8eec48b91d24ee22a05baaab9
Robin Cassan <robin.cassan@netc.eu>
2025-07-07 12:04:39 +0200

    premier commit java tp3

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,31 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    // déclaration de variables de différents type
+
+    public static void main(String[] arg){
+    byte a = 1;
+    short b = 2;
+    int c = 3;
+    long d = 4;
+    float e = 5;
+    double f = 6;
+    char g = 'a';
+    boolean h = true;
+    String i = "Hello world";
+    String randomString = "voici le résultat d'un calcul :\n 1 + 5 = 6";
+        // utilisez l'abréviation sout pour system.out.println();
+        System.out.println("Bonjour");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("c = "+ c);
+        System.out.println("d = "+ d);
+        System.out.println("e = "+ e);
+        System.out.println("f = "+ f);
+        System.out.println("g = "+ g);
+        System.out.println("h = "+ h);
+        System.out.println("i = "+ i);
+        System.out.println(randomString);
+
+    }
+}
